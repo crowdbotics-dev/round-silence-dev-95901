794bee7eae3937da8687c5557d8c4af6ff2f36b8
Crowdbotics <engineering@crowdbotics.com>
2023-06-29 10:27:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "round-silence-dev-95901",
+  url: "https://round-silence-dev-95901.botics.co",
+  api: "https://round-silence-dev-95901.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
